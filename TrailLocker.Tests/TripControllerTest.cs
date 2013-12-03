97cf6328ebd08bd88344dbb201458dbd17473fa3
Andrew Smith <andrew_smith3@taylor.edu>
2013-12-02 21:32:27 -0500

    Added TripControllerTest class

---
+++ TrailLocker.Tests/TripControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrailLocker.Tests
+{
+    [TestClass]
+    public class TripControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
